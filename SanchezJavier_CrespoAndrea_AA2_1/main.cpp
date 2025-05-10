0a37f34487e0fe9e82c4250ea3b30ae572f10047
AndreaCrespoOlarte <andrea.crespo@enti.cat>
2025-05-10 14:31:41 +0200

    Randomized NPC movement

--- SanchezJavier_CrespoAndrea_AA2_1/main.cpp
+++ SanchezJavier_CrespoAndrea_AA2_1/main.cpp
@@ -7,6 +7,64 @@ void UpdateScreen ( Map & map , Player & player )
     system ( "cls" );
     map.DrawFieldOfView ( player );
 }
+
+void NPCMovement(Map map)
+{
+    int option = rand() % 4;
+    
+    for (int i = 0; i < map.GetPedestrianSFList().size() - 1; i++)
+    {
+        Vector2 vector = map.GetPedestrianSFList()[i].GetPosition();
+        do
+        {
+            switch (option)
+            {
+            case 0: //arriba
+                vector.y--;
+                break; //abajo
+            case 1:
+                vector.y++;
+                break;
+            case 2: //derecha
+                vector.x++;
+                break;
+            case 3: //izquierda
+                vector.x--;
+                break;
+            default:
+                break;
+            }
+        } while (!map.IsValidPosition(vector) && vector != map.GetPedestrianSFList()[i].GetPosition());
+        map.GetPedestrianSFList()[i].SetPosition(vector.x,vector.y);
+    }
+
+    for (int i = 0; i < map.GetPedestrianLSList().size() - 1; i++)
+    {
+        Vector2 vector = map.GetPedestrianLSList()[i].GetPosition();
+        do
+        {
+            switch (option)
+            {
+            case 0: //arriba
+                vector.y--;
+                break; //abajo
+            case 1:
+                vector.y++;
+                break;
+            case 2: //derecha
+                vector.x++;
+                break;
+            case 3: //izquierda
+                vector.x--;
+                break;
+            default:
+                break;
+            }
+        } while (!map.IsValidPosition(vector) && vector != map.GetPedestrianLSList()[i].GetPosition());
+        map.GetPedestrianLSList()[i].SetPosition(vector.x, vector.y);
+    }
+}
+
 void main()
 {
 
@@ -24,6 +82,7 @@ void main()
 		//INPUTS
         if ( GetAsyncKeyState ( VK_UP ) & 0x8000 )
         {
+            NPCMovement(map);
             Vector2 newPos = { player.GetPosition ( ).x, player.GetPosition ( ).y - 1 };
             if ( map.IsValidPosition ( newPos ) )
             {
@@ -37,6 +96,7 @@ void main()
 
         else if ( GetAsyncKeyState ( VK_DOWN ) & 0x8000 )
         {
+            NPCMovement(map);
             Vector2 newPos = { player.GetPosition ( ).x, player.GetPosition ( ).y + 1 };
             if ( map.IsValidPosition ( newPos ) )
             {
@@ -50,6 +110,7 @@ void main()
 
         else if ( GetAsyncKeyState ( VK_RIGHT ) & 0x8000 )
         {
+            NPCMovement(map);
             Vector2 newPos = { player.GetPosition ( ).x + 1, player.GetPosition ( ).y };
             if ( map.IsValidPosition ( newPos ) )
             {
@@ -63,6 +124,7 @@ void main()
 
         else if ( GetAsyncKeyState ( VK_LEFT ) & 0x8000 )
         {
+            NPCMovement(map);
             Vector2 newPos = { player.GetPosition ( ).x - 1, player.GetPosition ( ).y };
             if ( map.IsValidPosition ( newPos ) )
             {
